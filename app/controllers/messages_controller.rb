6c4485674ac6774dbd76dd65820cf56e4052662c
Christina Sugiono <christina.sugiono95@gmail.com>
2022-04-25 18:20:03 +0800

    rails chatbox

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,9 +4,12 @@ class MessagesController < ApplicationController
     @message = Message.new(message_params)
     @message.chatroom = @chatroom
     @message.user = current_user
-    binding.pry
     if @message.save
-      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
+      ChatroomChannel.broadcast_to(
+        @chatroom,
+        render_to_string(partial: "message", locals: {message: @message})
+      )
+      head :ok
     else
       render "chatrooms/show"
     end
